4f90cf4ba2f6582a0eff6d2b70558be99cd8e1bb
José Bernal <jbernal.web.dev@gmail.com>
2024-07-30 08:47:22 -0700

    Remove methods related to outlining into the OrsOutline class.

--- src/OrsChapter.js
+++ src/OrsChapter.js
@@ -1,82 +1,59 @@
-export { OrsChapter };
+import OrsOutline from './Outline.js';
 
 const gSubRe = /^\(([0-9a-zA-Z]+)\)(.*)/gm;
 const subRe = /^\(([0-9a-zA-Z]+)\)(.*)/;
 
-class OrsChapter {
-    static cache = {};
-
-    // Class variables
+// Fetches the contents of the original ORS chapter from the Oregon Legislature web site.
+// Transforms it in to a well-formed HTML document.
+export default class OrsChapter {
+    // The chapter number.
     chapterNum = null;
+
+    // Title of this chapter - must be a string.
+    title;
 
     // The chapter's underlying XML document.
     doc = null;
 
-    docTwo = null;
-
-    // What is the difference between these two variables.
+    // Parsed title of each section of this chapter.
     sectionTitles = {};
 
+    // Contains references to DOM node <b> elements.
+    // Might be unused.
     sectionHeadings = {};
-
-    // Boolean indicating if the chapter's XML document has been loaded.
-    loaded = false;
-
-    initialized = false;
-
-    // Boolean indicating if the chapter's XML document has been modified.
-    injected = false;
-
-    doneBuilding = false;
 
     constructor(chapterNum) {
         this.chapterNum = chapterNum;
+        this.doc = new Document();
     }
 
-    static async fromCache(chapter, resp) {
-        if (OrsChapter.cache[chapter]) {
-            return OrsChapter.cache[chapter];
-        }
+    // Convert one unstructured chapter into a structured chapter.
+    // Use the anchors in the unstructured chapter to build a structured chapter
+    // where each section and subsection(s) are grouped and wrapped in the appropriate node hierarchy.
+    static toStructuredChapter(chapter) {
+        let ch = new OrsChapter(chapter.chapterNum);
+        let doc = ch.doc;
+        ch.chapterTitle = chapter.chapterTitle;
+        ch.sectionTitles = chapter.sectionTitles;
 
-        let ch = new OrsChapter(chapter);
-
-        let p = new Promise(async (resolve, reject) => {
-            await ch.load(resp);
-            ch.init();
-            resolve(ch);
-        });
-
-        OrsChapter.cache[chapter] = p;
-
-        return p;
-    }
-
-    // Outputs the document as an HTML string
-    toString() {
-        const serializer = new XMLSerializer();
-        const subset = this.doc.querySelector('.WordSection1');
-        return serializer.serializeToString(subset);
-    }
-
-    init() {
-        this.docTwo = new Document();
-        let wordSection = this.docTwo.createElement('div');
+        let wordSection = doc.createElement('div');
         wordSection.setAttribute('class', 'WordSection1');
 
-        for (var prop in this.sectionTitles) {
+        for (var prop in chapter.sectionTitles) {
+            // Create a new section element.
+            const section = doc.createElement('div');
+            section.setAttribute('id', 'section-' + prop);
+
             // console.log(prop);
             let startId = 'section-' + parseInt(prop);
-            let endId = this.getNextSectionId(startId);
-            let header, matches;
-            let clonedSection = this.cloneFromIds(startId, endId);
-            [header, matches] = this.retrievePTags(clonedSection);
+            let endId = chapter.getNextSectionId(startId);
+            let clonedSection = chapter.cloneFromIds(startId, endId);
+            let [header, matches] = chapter.retrievePTags(clonedSection);
 
-            const section = this.docTwo.createElement('div');
-
-            section.setAttribute('id', 'section-' + prop);
-            //if matches are returned as just a string which means no subsections exist for that section then you just build the element with the text that is stored in matches and append it to the section
+            // If matches are returned as just a string which means no subsections exist for that section then you just build the element with the text that is stored in matches and append it to the section
             if (typeof matches == 'string') {
-                let element = this.buildElement(
+                let element = OrsOutline.buildSection(
+                    doc,
                     'description',
                     'section-' + prop + '-description',
                     matches,
@@ -84,43 +61,13 @@ class OrsChapter {
                 );
                 section.appendChild(element);
             } else {
-                this.iterateMatches(matches, 0, section, prop);
+                chapter.iterateMatches(matches, 0, section, prop);
             }
             wordSection.appendChild(section);
         }
-        this.docTwo.appendChild(wordSection);
-        console.log(this.docTwo);
-    }
+        doc.appendChild(wordSection);
 
-    // Fetches the contents of the original ORS chapter from the Oregon Legislature web site.
-    // Transforms it in to a well-formed HTML document.
-    async load(resp) {
-        console.log('CALLING LOAD');
-        if (this.loaded) {
-            console.log('LOAD ALREADY COMPLETED');
-            return Promise.resolve(this.doc);
-        }
-
-        return resp
-            .arrayBuffer()
-            .then(function (buffer) {
-                const decoder = new TextDecoder('iso-8859-1');
-                return decoder.decode(buffer);
-            })
-            .then(html => {
-                const parser = new DOMParser();
-
-                // Tell the parser to look for html
-                this.doc = parser.parseFromString(html, 'text/html');
-
-                if (!this.formatted) {
-                    this.parse();
-                    this.injectAnchors();
-                }
-
-                this.loaded = true;
-                return this.doc;
-            });
+        return ch;
     }
 
     static fromResponse(resp, chapterNum) {
@@ -137,12 +84,35 @@ class OrsChapter {
                 // Tell the parser to look for html
                 chapter.doc = parser.parseFromString(html, 'text/html');
 
-                chapter.parse();
+                let [sectionTitles, sectionHeadings] =
+                    OrsOutline.retrieveSectionTitles(chapter.doc);
+                chapter.sectionTitles = sectionTitles;
+                chapter.sectionHeadings = sectionHeadings;
                 chapter.injectAnchors();
 
-                chapter.loaded = true;
                 return chapter;
             });
+    }
+
+    // Inserts anchors as <div> tags in the doc.
+    // Note: this affects the underlying structure
+    //  of the XML document.
+    injectAnchors() {
+        for (var prop in this.sectionTitles) {
+            let headingDiv = this.doc.createElement('div');
+            headingDiv.setAttribute('id', 'section-' + prop);
+            headingDiv.setAttribute('class', 'ocdla-heading');
+            headingDiv.setAttribute('data-chapter', this.chapterNum);
+            headingDiv.setAttribute('data-section', prop);
+
+            let target = this.sectionHeadings[prop];
+            target.parentNode.insertBefore(headingDiv, target);
+        }
+        var subset = this.doc.querySelector('.WordSection1');
+        let headingDiv = this.doc.createElement('div');
+        headingDiv.setAttribute('class', 'ocdla-heading');
+        headingDiv.setAttribute('id', 'end');
+        subset.appendChild(headingDiv);
     }
 
     /**
@@ -151,7 +121,7 @@ class OrsChapter {
      * @returns DOMNode
      */
     getSection(id) {
-        return this.docTwo.getElementById('section-' + id);
+        return this.doc.getElementById('section-' + id);
     }
 
     /**
@@ -232,7 +202,7 @@ class OrsChapter {
     static parseSubsections(reference) {
         let subs = reference.match(/(?<=\()([0-9a-zA-Z]+)(?=\))/g);
 
-        console.log(subs);
+        console.log('parseSubsections()', subs);
 
         return subs;
     }
@@ -295,7 +265,7 @@ class OrsChapter {
 
         matches = matches.match(gSubRe);
 
-        return matches == null ? [header, text] : [header, matches];
+        return matches === null ? [header, text] : [header, matches];
     }
 
     iterateMatches(
@@ -333,7 +303,7 @@ class OrsChapter {
         } else {
             id = match[1];
             text = '(' + id + ')' + match[2];
-            level = this.findLevel(id, nextMatch);
+            level = OrsOutline.findLevel(id, nextMatch);
         }
 
         //console.log(match);
@@ -358,7 +328,7 @@ class OrsChapter {
             return;
         }
         divId = parent.getAttribute('id') + '-' + id;
-        let element = this.buildElement(id, divId, text, level);
+        let element = OrsOutline.buildSection(this.doc, id, divId, text, level);
         parent.appendChild(element);
         // identify subsections
         // build subsection grouping elements
@@ -372,116 +342,12 @@ class OrsChapter {
         );
     }
 
-    buildElement(id, divId, text, level) {
-        let sub = this.docTwo.createElement('div');
-        sub.setAttribute('id', divId);
-        sub.setAttribute('class', 'level-' + level);
-
-        let span = this.docTwo.createElement('span');
-        span.setAttribute('class', 'subsection');
-
-        if (id != 'description') {
-            span.innerText = '(' + id + ')';
+    removeNodes(selector) {
+        let nodes = this.doc.querySelectorAll(selector);
+        for (var i = 0; i < nodes.length; i++) {
+            let node = nodes[i];
+            node.parentNode.removeChild(node);
         }
-
-        let theText = this.docTwo.createTextNode(text);
-
-        sub.appendChild(span);
-        sub.appendChild(theText);
-
-        return sub;
-    }
-
-    findLevel(text, nextMatch) {
-        let subNumRe = /^[0-9]+/;
-        let subUpperRe = /^[A-Z]+/;
-        let subRe = /^\(([0-9a-zA-Z]+)\)(.*)/;
-
-        let nextId;
-
-        if (nextMatch != null) {
-            nextId = nextMatch.match(subRe)[1];
-        }
-
-        if (text.match(subNumRe)) {
-            return '0';
-        } else if (
-            !this.isRomanNumeral(text, nextId) &&
-            !text.match(subUpperRe)
-        ) {
-            return '1';
-        } else if (text.match(subUpperRe)) {
-            return '2';
-        } else if (this.isRomanNumeral(text, nextId)) {
-            return '3';
-        }
-    }
-
-    isRomanNumeral(text, nextText) {
-        let romanReg = /^[ivx]+/;
-        if (nextText == null) {
-            return text.match(romanReg);
-        }
-        return (
-            text.match(romanReg) &&
-            (nextText.match(romanReg) || text.length > 1)
-        );
-    }
-
-    parse() {
-        // Createa nodeList of all the <b> elements in the body
-        let headings = this.doc.querySelectorAll('b');
-
-        for (var i = 0; i < headings.length; i++) {
-            let boldParent = headings[i];
-            var trimmed = headings[i].textContent.trim();
-            if (trimmed.indexOf('Note') === 0) continue;
-            let strings = trimmed.split('\n');
-            let chapter, section, key, val;
-
-            // If array has only one element,
-            // Then we know this doesn't follow the traditional statute pattern.
-            if (strings.length === 1) {
-                key = strings[0];
-                val = boldParent.nextSibling
-                    ? boldParent.nextSibling.textContent
-                    : '';
-            } else {
-                // otherwise our normal case.
-                key = strings[0];
-                val = strings[1];
-
-                let numbers = key.split('.');
-                chapter = numbers[0];
-                section = numbers[1];
-            }
-
-            // Might need to change this one to remove parseInt
-            this.sectionTitles[parseInt(section)] = val;
-            this.sectionHeadings[parseInt(section)] = boldParent;
-        }
-    }
-
-    // Inserts anchors as div tags in the doc.
-    // Note: this affects the underlying structure
-    //  of the XML document.
-    injectAnchors() {
-        for (var prop in this.sectionTitles) {
-            let headingDiv = this.doc.createElement('div');
-            headingDiv.setAttribute('id', 'section-' + prop);
-            headingDiv.setAttribute('class', 'ocdla-heading');
-            headingDiv.setAttribute('data-chapter', this.chapterNum);
-            headingDiv.setAttribute('data-section', prop);
-
-            let target = this.sectionHeadings[prop];
-            target.parentNode.insertBefore(headingDiv, target);
-        }
-        var subset = this.doc.querySelector('.WordSection1');
-        let headingDiv = this.doc.createElement('div');
-        headingDiv.setAttribute('class', 'ocdla-heading');
-        headingDiv.setAttribute('id', 'end');
-        subset.appendChild(headingDiv);
-        this.formatted = true;
     }
 
     buildToc() {
@@ -576,5 +442,13 @@ class OrsChapter {
                 return nextSection.getAttribute('id');
             }
         }
+    }
+
+    // Outputs the document as an HTML string
+    toString() {
+        const serializer = new XMLSerializer();
+        const subset = this.doc.querySelector('.WordSection1');
+
+        return serializer.serializeToString(subset);
     }
 }
