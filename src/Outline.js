491e75e0f6a74ddb72209838f7826a8918805c14
José Bernal <jbernal.web.dev@gmail.com>
2024-09-09 13:30:32 -0700

    Parser update.

--- src/Outline.js
+++ src/Outline.js
@@ -4,7 +4,7 @@
  */
 
 
-const OUTLINE_LEVELS = {};
+const OUTLINE_LEVELS = [];
 
 // Some ORS subsections are empty and contain content only for their subsections.
 // These are expressed syntactically as "(3)(a)" or "(a)(1)".  We might even imagine they could be found as "(3)(a)(1)".
@@ -12,24 +12,32 @@ const OUTLINE_LEVELS = {};
 // An alternate, shorter form of the above which would also match "(3)(a)(1)";
 // whereas the abhove only captures two adjacent subsections.
 export const REGEX_ENUMERATIVE_PAIR = /(^\([0-9a-zA-Z]+\)){2,}/gm;
+
 export const REGEX_ENUMERATIVE = /^\(([0-9a-zA-Z]+)\)(.*)/;
 
-// Level 0 should refer to the section itself;
+// Level 0 should refer to the chapter itself;
 // For placeholder purposes only. Never used.
 const LEVEL_0_REGEX = null;
 
+// Level 1 is marked by numbers for sections and - for now - should not be used.
+const LEVEL_1_REGEX = null;
+
 // Level 1 is marked by numbers and takes the form of "(1)" or "(2)".
-const LEVEL_1_REGEX = "[0-9]+";
+const LEVEL_2_REGEX = "[0-9]+";
 
 // Level 2 is marked by lowercase letters and takes the form of "(a)" or "(b)".
-const LEVEL_2_REGEX = "[a-z]+";
+const LEVEL_3_REGEX = "[a-z]+";
 
 // Level 3 is marked by uppercase letters and takes the form of "(A)" or "(B)".
-const LEVEL_3_REGEX = "[A-Z]+";
+const LEVEL_4_REGEX = "[A-Z]+";
 
 // Level 4 is marked by small roman numerals and takes the form of "(i)" or "(ii)".
 // 1, 5, 10, 50, 100, 500, and 1,000 are represented by the characters i, v, x, l, c, d, and m respectively.
-const LEVEL_4_REGEX = "[ivxlcdm]+";
+const LEVEL_5_REGEX = "[ivxlcdm]+";
+
+// Level 4 is marked by small roman numerals and takes the form of "(i)" or "(ii)".
+// 1, 5, 10, 50, 100, 500, and 1,000 are represented by the characters i, v, x, l, c, d, and m respectively.
+const LEVEL_6_REGEX = "[IVXLCDM]+";
 
 export const OUTLINE_LEVEL_0 = 0;
 
@@ -41,8 +49,35 @@ export const OUTLINE_LEVEL_3 = 3;
 
 export const OUTLINE_LEVEL_4 = 4;
 
+export const OUTLINE_LEVEL_5 = 5;
+
+export const OUTLINE_LEVEL_6 = 6;
+
 export const OUTLINE_LEVEL_UNKNOWN = -1;
 
+const CHAPTER_NUMERAL = 0;
+
+const SECTION_NUMERAL = 1;
+
+const SUBSECTION_NUMERAL = 2;
+
+const SUBSECTION_LOWERCASE = 3;
+
+const SUBSECTION_UPPERCASE = 4;
+
+const SUBSECTION_SMALL_ROMAN_NUMERAL = 5;
+
+const SUBSECTION_BIG_ROMAN_NUMERAL = 6;
+
+const SAMPLE_PATH = [
+  CHAPTER_NUMERAL,
+  SECTION_NUMERAL,
+  SUBSECTION_NUMERAL,
+  SUBSECTION_LOWERCASE,
+  SUBSECTION_UPPERCASE,
+  SUBSECTION_SMALL_ROMAN_NUMERAL,
+  SUBSECTION_BIG_ROMAN_NUMERAL,
+];
 
 
 OUTLINE_LEVELS[OUTLINE_LEVEL_0] = LEVEL_0_REGEX;
@@ -50,29 +85,107 @@ OUTLINE_LEVELS[OUTLINE_LEVEL_1] = LEVEL_1_REGEX;
 OUTLINE_LEVELS[OUTLINE_LEVEL_2] = LEVEL_2_REGEX;
 OUTLINE_LEVELS[OUTLINE_LEVEL_3] = LEVEL_3_REGEX;
 OUTLINE_LEVELS[OUTLINE_LEVEL_4] = LEVEL_4_REGEX;
+OUTLINE_LEVELS[OUTLINE_LEVEL_5] = LEVEL_5_REGEX;
+OUTLINE_LEVELS[OUTLINE_LEVEL_6] = LEVEL_6_REGEX;
+
+const OP_REFERENCE_SEPARATOR = ",";
+
+const OP_REFERENCE_RANGE = "-";
+
+const CHAR_REFERENCE_SPACE = " ";
+
+const CHAR_SUBSECTION_OPEN = "(";
+
+const CHAR_SUBSECTION_CLOSE = ")";
+
+const CHAR_CHAPTER_SECTION_SEPARATOR = ".";
+
+const CHAR_HYPHEN_SEPARATOR = "-";
+
+
+/**
+let text = "138.5(3)(a),(4)(a)-(c)";
+Parser.parseReferences(text);
+ */
+export function parseReferences(refs) {
+  return refs
+    .split(OP_REFERENCE_SEPARATOR)
+    .map((ref) => {return ref.includes(OP_REFERENCE_RANGE) ? ref.split(OP_REFERENCE_RANGE) : ref})
+    .map(ref => { return Array.isArray(ref) ? ref.map(_parseReferences) : _parseReferences(ref); })
+    .map((partial) => {
+      return Array.isArray(partial[0]) ? partial.map(toMatrix) : toMatrix(partial);
+  });
+}
+
+
+function toMatrix(parts) {
+  let tmp = parts.map((part) => toPath(part));
+  let matrix = tmp.reduce((acc, val) => addPaths(acc, val));
+  console.log(matrix);
+  return matrix;
+}
+
+export function _parseReferences(ref) {
+  let regex = /(?<=\()(\w+)(?=\))+?/g;
+  return ref.match(regex);
+}
+
+export function toPath(value, comparison = null) {
+  let matrix = SAMPLE_PATH.slice().fill(null);
+  let position = comparison ? Outline.getPosition(value,comparison) : Outline.getPosition(value);
+  matrix[position] = value;
+
+  return matrix;
+}
+
+export function addPaths(matrix1, matrix2) {
+  let result = matrix1.slice();
+  for (var i = 0; i < matrix1.length; i++) {
+    result[i] = matrix2[i] || matrix1[i];
+  }
+
+  return result;
+}
+
+
+
 
 
 
 
 
 export default class Outline {
+  static getPosition(str, comparison = null) {
+
+    for (let level = (comparison ? Outline.getPosition(comparison)+1 : OUTLINE_LEVEL_2); level < OUTLINE_LEVELS.length; level++) {
+      if(!OUTLINE_LEVELS[level]) continue;
+      let regex = new RegExp(`^(?<label>${OUTLINE_LEVELS[level]})`);
+      let matches = str.match(regex);
+      // console.log(matches);
+      if (matches) {
+        return parseInt(level);
+      }
+    }
+    return parseInt(OUTLINE_LEVEL_UNKNOWN);
+  }
+
   static matchLevel(str) {
-    for (const LEVEL in OUTLINE_LEVELS) {
-      let regex = new RegExp(`^\\((?<label>${OUTLINE_LEVELS[LEVEL]})\\)`);
+    for (let level = OUTLINE_LEVEL_2; level < OUTLINE_LEVELS.length; level++) {
+      let regex = new RegExp(`^\\((?<label>${OUTLINE_LEVELS[level]})\\)`);
       let matches = str.match(regex);
       console.log(matches);
       if (matches) {
-        return [parseInt(LEVEL),matches.groups.label];
+        return [parseInt(level), matches.groups.label];
       }
     }
-    return [OUTLINE_LEVEL_UNKNOWN,str];
+    return [OUTLINE_LEVEL_UNKNOWN, str];
   }
 
   static assignLevel(str, previousLevel = OUTLINE_LEVEL_UNKNOWN) {
-    let [level,label] = Outline.matchLevel(str);
+    let [level, label] = Outline.matchLevel(str);
 
     if (level == OUTLINE_LEVEL_4 && previousLevel == OUTLINE_LEVEL_2) {
-      return [OUTLINE_LEVEL_2,label];
-    } else return [level,label];
+      return [OUTLINE_LEVEL_2, label];
+    } else return [level, label];
   }
 }
